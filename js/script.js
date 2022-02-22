3582061fc4a8a15db56886d2c6060befa29dc4d5
Darshan Damre <darshandamre5@gmail.com>
2022-02-22 20:57:56 +0530

    added persisting theme

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 const toggleBtn = document.querySelector(".toggle-theme");
+const themeIcon = document.querySelector(".toggle-theme > i");
+const userPreferenceTheme = localStorage.getItem("theme");
+
+const enableLightMode = () => {
+  document.body.classList.toggle("light-mode");
+  themeIcon.classList.remove("fa-sun");
+  themeIcon.classList.add("fa-moon");
+  localStorage.setItem("theme", "light-mode");
+};
+
+const enableDarkMode = () => {
+  document.body.classList.toggle("light-mode");
+  themeIcon.classList.remove("fa-moon");
+  themeIcon.classList.add("fa-sun");
+  localStorage.setItem("theme", "dark-mode");
+};
+
+if (userPreferenceTheme === "light-mode") {
+  enableLightMode();
+}
 
 toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("light-mode");
+  console.log(themeIcon.classList.contains("fa-sun"));
+  if (themeIcon.classList.contains("fa-sun")) {
+    enableLightMode();
+  } else {
+    enableDarkMode();
+  }
 });
